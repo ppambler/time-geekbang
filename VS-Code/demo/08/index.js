ab4fc811cd878cf7ef0052f74853ff9e14932a05
Ambler <allinlbj@gmail.com>
2019-02-14 16:52:45 +0800

    fi-01-08

---
+++ VS-Code/demo/08/index.js
@@ -0,0 +1,35 @@
+function foo(xx) {
+    bar("Hello World");
+    return undefined
+}
+
+function bar() {
+    return ''
+}
+foo()
+
+function bar() {
+
+}
+
+function bar() {
+
+}
+
+function bar() {
+
+}
+
+var o = {}
+
+var a = []
+
+var s = "String"
+
+var n = 1
+
+var c = undefined
+
+var e = null
+
+var t = true
