582384c89074c8b64711c666b2d6a8d24f439b8c
Ambler <allinlbj@gmail.com>
2019-07-24 01:43:57 +0800

    代码自动补全？单词快速修复和重构名字、提取代码为函数

--- VS-Code/demo/09/main.js
+++ VS-Code/demo/09/main.js
@@ -1,13 +1,62 @@
 function foo() {
-    bar("Hello World");
+    hello("Hello World");
 }
 
 foo()
 
-function bar(a) {
-    console.log()
+function hello(a) {
+    createDiv();
 
 
 }
+bar3()
 var arr = [1, '2', 3]
 arr.forE
+newFunction_1();
+
+function newFunction_1() {
+    console.log();
+}
+
+function createDiv() {
+    console.log();
+}
+
+function aa() {
+
+}
+
+aa()
+function xx() {
+
+}
+xx
+function dd() {
+
+}
+
+dd()
+
+function kk(a,b) {
+
+}
+
+kk(1, 2)
+
+kk(d8, 8)
+
+kk(\, 7)
+
+createDiv();
+
+function createDiv() {
+    console.log(1);
+}
+
+createDiv();
+
+function createDiv() {
+    let div = document.createElement(div);
+    document.body.appendChild(div);
+}
+document.body.appendChild(newChild)
