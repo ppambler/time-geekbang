1449b6aea60975651655af85a229107720c6013e
Ambler <allinlbj@gmail.com>
2019-08-03 01:09:13 +0800

    完成 VS Code 之极速搜索

--- VS-Code/demo/09/main.js
+++ VS-Code/demo/09/main.js
@@ -1,10 +1,10 @@
-function foo() {
+fuction foo() {
     hello("Hello World");
 }
 
 foo()
 
-function hello(a) {
+fuction hello(a) {
     createDiv();
 
 
@@ -12,32 +12,32 @@ function hello(a) {
 bar3()
 var arr = [1, '2', 3]
 arr.forE
-newFunction_1();
+newfuction_1();
 
-function newFunction_1() {
-    console.log();
+fuction newfuction_1() {
+    dd.log();
 }
 
-function createDiv() {
-    console.log();
+fuction createDiv() {
+    dd.log();
 }
 
-function aa() {
+fuction aa() {
 
 }
 
 aa()
-function xx() {
+fuction xx() {
 
 }
 xx
-function dd() {
+fuction dd() {
 
 }
 
 dd()
 
-function kk(a,b) {
+fuction kk(a,b) {
 
 }
 
@@ -49,39 +49,43 @@ kk(\, 7)
 
 createDiv();
 
-function createDiv() {
-    console.log(1);
+fuction createDiv() {
+    d.log(1);
 }
+
+hello
+
+HelloWorld
 
 createDiv();
 
-function createDiv() {
+fuction createDiv() {
     let div = document.createElement(div);
     document.body.appendChild(div);
 }
 document.body.appendChild(newChild)
-console.log('我的天啊');
-console.log();
-console.log();
+dd.log('我的天啊');
+dd.log();
+dd.log();
 
-console.log();
+dd.log();
 
-console.log('我的天啊');
+dd.log('我的天啊');
 
 
-console.log('你在顽皮');
+dd.log('你在顽皮');
 
 '我的饿去你'.log
-console.log('dee ');
+dd.log('dee ');
 饿哦
-console.log('');
-console.log('dee dewo ');
-console.log(i);
-console.log(的哦);
+dd.log('');
+dd.log('dee dewo ');
+adasdad.log(i);
+dsdad.log(的哦);
 
-console.log('');
+dd.log('');
 
-console.log('wode ');
+d.log('wode ');
 wo de ni zai wang shi pia 
 我的天
 
@@ -97,4 +101,32 @@ for (let i = 0; i < array.length; i++) {
 for (let i  = 0; i  < array.length; i ++) {
     const element = array[i ];
     
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+dada.log('我的天');
+我的
+
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+
+    dsdad.log('');
+    
+}
+dadadadad.log('wo ');
+wo 
+
+dd.loga('![1564024471086](img/10/1564024471086.png)');
+
+
+fuction addOne(aa,bbb) {
+    var aa = 1
+    var b = 3
+    cc = 5z
 }
