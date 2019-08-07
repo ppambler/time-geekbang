26b066a6bb6aa4d2773516b61381c8970027ec80
Ambler <allinlbj@gmail.com>
2019-08-08 00:32:07 +0800

    update 优化编辑设置

---
+++ VS-Code/demo/11/xx.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="xx"></div>
+</template>
+
+<script>
+export default {
+  name: "xx",
+  dadad: "dd"
+  dd: 'ff',
+  methods: {
+    add() {
+      var a = 1
+      var c = 2
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.xx {
+  display: flex;
+  position: relative;
+}
+</style>
